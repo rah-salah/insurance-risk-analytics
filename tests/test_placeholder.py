28bf666bd90523c56c0ade63494dd8d6e4d64def
rah-salah <rahmasalah6883@gmail.com>
2026-05-21 15:04:24 +0300

    fix: fix test to not check data folder

--- tests/test_placeholder.py
+++ tests/test_placeholder.py
@@ -1,11 +1,12 @@
-def test_imports():
-    import pandas as pd
-    import numpy as np
-    assert pd.__version__ is not None
-    assert np.__version__ is not None
-
-def test_structure():
-    import os
-    assert os.path.exists('data')
-    assert os.path.exists('notebooks')
-    assert os.path.exists('tests')
+def test_imports(): 
+    import pandas as pd 
+    import numpy as np 
+    assert pd.__version__ is not None 
+    assert np.__version__ is not None 
+ 
+def test_structure(): 
+    import os 
+    assert os.path.exists('notebooks') 
+    assert os.path.exists('tests') 
+    assert os.path.exists('src') 
+    assert os.path.exists('requirements.txt') 
